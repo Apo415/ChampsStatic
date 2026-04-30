3021a61f8ed026f9f3937b40cf2a29de2b1c41a0
Apo415 <c64701961@gmail.com>
2026-04-30 10:39:34 +0000

    création des instances de la classe Personne

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,12 @@
 public class Main {
     public static void main(String[] args) {
 
+        // création plusieurs instances de la classe Personne
+        Personne personne1 = new Personne();
+        Personne personne2 = new Personne();
+        Personne personne3 = new Personne();
+        Personne personne4 = new Personne();
+
+        System.out.println("(" + personne4.nbLocal + "," + Personne.nbInstances + ")");
     }
 }
